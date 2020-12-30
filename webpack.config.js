8b524e66c805847ffac135637b66eecb5c296945
Shuntaro Nishizawa <me@shun.technology>
2020-12-30 21:38:11 +0900

    build: add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
     ],
   },
